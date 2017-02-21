6043b18c473e841ad35dd8869cc8345123542d13
Alain Vaucher <alain.vaucher@phys.chem.ethz.ch>
2017-02-21 10:41:30 +0100

    Add function to create backup orbital files

--- mix_turbomole.py
+++ mix_turbomole.py
@@ -1,1 +1,9 @@
 from shutil import copyfile
+
+def create_orbital_backup(directory):
+    """
+    Creates .bak files for the orbital files 'alpha' and 'beta'
+    :param directory: calculation directory
+    """
+    copyfile(directory + '/alpha', directory + '/alpha.bak')
+    copyfile(directory + '/beta', directory + '/beta.bak')
