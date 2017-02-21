5ae0625743879c3480e45e8e0a672123671baac2
Alain Vaucher <alain.vaucher@phys.chem.ethz.ch>
2017-02-21 12:53:27 +0100

    getting the arguments for the directory name

--- mix_turbomole.py
+++ mix_turbomole.py
@@ -1,3 +1,4 @@
+import sys
 from shutil import copyfile
 
 def create_orbital_backup(directory):
@@ -7,3 +8,21 @@ def create_orbital_backup(directory):
     """
     copyfile(directory + '/alpha', directory + '/alpha.bak')
     copyfile(directory + '/beta', directory + '/beta.bak')
+
+def read_number_electrons(directory):
+    """
+    :param directory:
+    :return:
+    """
+    control_file = directory + '/control'
+
+
+args = sys.argv
+if len(args) != 2:
+    print("Usage: python mix_turbomole.py <calculation_directory>")
+    sys.exit()
+
+calculation_directory = args[1]
+create_orbital_backup(calculation_directory)
+
+
