a10b3e155278428266c0402a0bd77f8125b98c0c
Alain Vaucher <alain.vaucher@phys.chem.ethz.ch>
2017-02-24 09:31:28 +0100

    add documentation in main_gaussian and improve the code

--- cpp/main_gaussian.cpp
+++ cpp/main_gaussian.cpp
@@ -4,23 +4,25 @@
 #include "OrbitalPerturbation/Mixing.h"
 #include <iostream>
 
-using namespace std;
-
+/*!
+ * Executable to mix the orbitals of Gaussian formatted checkpoint files.
+ * It reads a random seed from the file "seed" and then writes back to it to avoid generating the same random mixes between successive executions.
+ * Usage: [executable] [original formatted checkpoint file] [perturbed formatted checkpoint file]
+ *  f.i.: ./turbomoleOrbitalMixer checkpoint_original.fchk checkpoint_perturbed.fchk
+ */
 int main(int argc, char *argv[]) {
-  string current_exec_name = argv[0]; 
-  string chkFileName;
-  string newchkFileName;
+  std::string current_exec_name = argv[0];
+  std::string chkFileName;
+  std::string newchkFileName;
 
   if (argc < 3) {
-    cout << "Usage: <executable> <previous_checkpoint_file> <new_checkpoint_file>" << endl;
+    std::cout << "Usage: <executable> <original_checkpoint_file> <perturbed_checkpoint_file>" << std::endl;
     return 1;
   }
   else {
     chkFileName = argv[1];
     newchkFileName = argv[2];
   }
-
-  cout << "Reading the structure from the file " << chkFileName << "." << endl;
 
   OrbitalPerturbation::GaussianOrbitalFileReader gt(chkFileName);
 
